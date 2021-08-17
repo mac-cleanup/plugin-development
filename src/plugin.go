e46a1e3d93b9de50f9386dfb29af3f08d4fd6b8c
Florian Wartner <florian@wartner.io>
2021-08-18 00:14:31 +0200

    Initial

--- src/plugin.go
+++ src/plugin.go
@@ -3,19 +3,39 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+
+	color "github.com/fatih/color"
 )
 
 type plugin string
 
-var introMsg = "Cleaning up MY_FILES.." // This is the message that will be displayed to the user
+var introMsg = "Cleaning up Development Files.."
 
 func (g plugin) Cleanup() {
 	fmt.Println(introMsg)
-	DeleteFiles("~/DIR_YOU_WANT_TO_BE_CLEANED") // DIR_YOU_WANT_TO_BE_CLEANED is the path to the directory you want to be cleaned
+	DeleteFiles("~/.bash_history", "Bash history..")
+	DeleteFiles("~/.zhistory", "ZSH history..")
+	color.Cyan("Android caches..")
+	DeleteFiles("~/.android/cache", "Android caches..")
+	DeleteFiles("~/.gradle/caches", "")
+	DeleteFiles("~/wget-log", "wget history..")
+	DeleteFiles("~/.wget-hsts", "wget history..")
+	DeleteFiles("~/Library/Logs/JetBrains/*/", "JetBrains logs..")
+	DeleteFiles("~/Library/Developer/Xcode/DerivedData/*", "Xcode data..")
+	DeleteFiles("~/Library/Developer/Xcode/Archives/*", "")
+	DeleteFiles("~/Library/Developer/Xcode/iOS Device Logs/*", "")
+	ShellCommand("gem cleanup", "Gem files..")
+	ShellCommand("docker system prune -af", "Docker images..")
+	ShellCommand("brew cleanup -s", "Homebrew..")
+	ShellCommand("brew tap --repair", "")
+	ShellCommand("npm cache clean --force", "NPM cache..")
+	ShellCommand("yarn cache clean --force", "Yarn cache..")
+	ShellCommand("pod cache clean --all", "Cocoapods cache..")
 }
 
-func DeleteFiles(dir string) error {
+func DeleteFiles(dir string, message string) error {
 	d, err := os.Open(dir)
 	if err != nil {
 		return err
@@ -31,7 +51,22 @@ func DeleteFiles(dir string) error {
 			return err
 		}
 	}
+
+	color.Cyan(message + "...")
 	return nil
+}
+
+func main() {
+	plugin := plugin("")
+	plugin.Cleanup()
+}
+
+func ShellCommand(command string, message string) {
+	_, err := exec.Command("/bin/sh", "-c", command).Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	color.Cyan(message)
 }
 
 var Cleanup plugin
